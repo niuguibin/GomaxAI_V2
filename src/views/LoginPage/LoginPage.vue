9fb65c528099c9cc010cea43a6af6f63f74437b6
FluffyChi-Xing <121754413+FluffyChi-Xing@users.noreply.github.com>
2024-01-23 11:45:43 +0800

    第三次提交

--- src/views/LoginPage/LoginPage.vue
+++ src/views/LoginPage/LoginPage.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-import { reactive,ref } from "vue";
+import { reactive,ref,h } from "vue";
 import {useRouter} from "vue-router";
 import type { FormInstance, FormRules } from 'element-plus'
-import {ElMessage} from "element-plus";
+import {ElMessage, ElNotification} from "element-plus";
+import axios from "axios";
 const ruleFormRef = ref<FormInstance>()
 const router = useRouter()
 interface RuleForm {
@@ -45,6 +46,7 @@ const login_rules = reactive<FormRules<RuleForm>>({
 
 
 //注册
+//表单校验
 const registerFormRef = ref<FormInstance>()
 const register = () => {
   isShow_1.value = !isShow_1.value
@@ -60,10 +62,17 @@ const Register = reactive<registerForm>({
   password: '',
   code: ''
 })
+const login_statues = ref(100)
+//注册函数
 const registerSubmit = async (formEl: FormInstance | undefined) => {
   if (!formEl) return
   await formEl.validate((valid, fields) => {
-    if (valid) {
+    axios.get(`http://8.130.35.251:3000/captcha/verify?phone=${Register.username}&captcha=${Register.code}`).then((res) => {
+      console.log(res)
+      login_statues.value = res.data.code
+    })
+    if (valid && login_statues.value !== 503) {
+      //跳回登录页面
       isShow_1.value = !isShow_1.value
       isShow_2.value = !isShow_2.value
       ElMessage({
@@ -73,6 +82,10 @@ const registerSubmit = async (formEl: FormInstance | undefined) => {
       console.log('register success!')
     } else {
       console.log('error submit!', fields)
+      ElMessage({
+        message: '验证码错误!',
+        type: 'warning',
+      })
     }
   })
 }
@@ -82,7 +95,7 @@ const register_rules = reactive<FormRules<registerForm>>({
       required: true,message: '请输入用户名',trigger: 'blur'
     },
     {
-      min: 5,max: 15,message: '用户名长度是5到15',trigger: 'blur'
+      min: 11,max: 15,message: '用户名长度是11到15',trigger: 'blur'
     }
   ],
   password: [
@@ -96,11 +109,40 @@ const register_rules = reactive<FormRules<registerForm>>({
   code: [
     {
       required: true,message: '请输入验证码',trigger: 'blur'
+    },
+    {
+      min: 4,max: 6,message: '验证码的长度是4到6',trigger: 'blur'
     }
   ]
 })
 const isShow_1 = ref(true)
 const isShow_2 = ref(false)
+
+
+//验证码获取和校验
+const mes = ref('获取验证码')
+const number = ref(60)
+const captcha = () => {
+  if (Register.username !== '' && Register.password !== ''){
+    axios.get(`http://8.130.35.251:3000/captcha/sent?phone=${Register.username}`).then((res) => {
+      console.log(res)
+    })
+    const timer = setInterval(() => {
+      mes.value = `重新发送(${number.value})`
+      number.value --
+      if (number.value === 0){
+        clearInterval(timer)
+        mes.value = '重新发送'
+      }
+    },1000)
+  }
+  else {
+    ElNotification({
+      title: '警告',
+      message: h('i', { style: 'color: teal' }, '手机号和密码不能为空!'),
+    })
+  }
+}
 </script>
 
 <template>
@@ -124,8 +166,8 @@ const isShow_2 = ref(false)
             status-icon
             label-position="left"
         >
-          <el-form-item label="用户名:" prop="username">
-            <el-input clearable placeholder="请输入用户名" v-model="login.username"/>
+          <el-form-item label="手机号:" prop="username">
+            <el-input clearable placeholder="请输入手机号" v-model="login.username"/>
           </el-form-item>
           <el-form-item label="密码:" prop="password">
             <el-input type="password" clearable show-password v-model="login.password" placeholder="请输入密码"/>
@@ -152,8 +194,8 @@ const isShow_2 = ref(false)
             :model="Register"
             :rules="register_rules"
         >
-          <el-form-item label="用户名:" prop="username">
-            <el-input clearable placeholder="请输入用户名" v-model="Register.username"/>
+          <el-form-item label="手机号:" prop="username">
+            <el-input clearable placeholder="请输入手机号" v-model="Register.username"/>
           </el-form-item>
           <el-form-item label="密码:" prop="password">
             <el-input clearable placeholder="请输入密码" type="password" v-model="Register.password" show-password/>
@@ -161,7 +203,7 @@ const isShow_2 = ref(false)
           <el-form-item prop="code" style="margin-bottom: 10px">
             <el-input clearable v-model="Register.code" placeholder="请输入验证码"/>
           </el-form-item>
-          <el-button style="width: 80px;" class="el-button-2">获取验证码</el-button>
+          <el-button style="width: 80px;" class="el-button-2" @click="captcha">{{mes}}</el-button>
           <el-button class="el-button-1" round @click="registerSubmit(registerFormRef)">注册</el-button>
         </el-form>
       </div>
