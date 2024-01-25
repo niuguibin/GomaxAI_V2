90ba7e91ae1953b8bf4d9f43b94036563f977c1a
FluffyChi-Xing <121754413+FluffyChi-Xing@users.noreply.github.com>
2024-01-25 14:39:30 +0800

    第四次提交，初步完成了家族人生页面

--- src/views/FamilyLife/FamilyPage.vue
+++ src/views/FamilyLife/FamilyPage.vue
@@ -1,10 +1,103 @@
 <script setup>
-
+import {h, ref} from "vue";
+import { Position,PictureRounded } from "@element-plus/icons-vue";
+import {ElNotification} from "element-plus";
+const ai_info = ref([
+  {
+    id: '爷爷',
+    info: '我是爷爷'
+  },
+  {
+    id: '爸爸',
+    info: '我是爸爸'
+  },
+  {
+    id: '祖母',
+    info: '我是祖母'
+  },
+  {
+    id: '舅爷',
+    info: '我是舅爷'
+  }
+])
+const message = ref('')
+//该函数用于选择AI角色
+const isShow = ref(false)
+const info = ref('')
+const select_ai = (index) => {
+  info.value = ai_info.value[index].info
+  isShow.value = true
+}
+//该函数用于用户发送信息，发送信息前先判断是否选择角色，如果为否则通知用户先选角色
+const user_info = ref('')
+const show = ref(false)
+const commit =  () => {
+  //如果用户选择了角色，isShow为true
+  if (isShow.value && message.value !== ''){
+    user_info.value = message.value
+    show.value = true
+  }else{
+    ElNotification({
+      title: '警告',
+      message: h('i', { style: 'color: teal' }, '请先选择对话角色!'),
+    })
+  }
+}
 </script>
 
 <template>
   <div class="background">
-    我是家族人生页面
+    <div class="inner-box">
+      <div class="left-box">
+        <div class="ai-people" v-for="(item,index) in ai_info" :key="index" @click="select_ai(index)">
+          <img src="../../assets/user.png" alt="" loading="lazy">
+          <div class="ai-info">
+            <p>{{item.id}}</p>
+            <p>{{item.info}}</p>
+          </div>
+        </div>
+      </div>
+      <div class="right-box">
+        <div class="message">
+          <div class="p-talking" v-show="isShow">
+            <div class="ai-dot">
+              <img src="../../assets/user.png" alt="" loading="lazy">
+            </div>
+            <div class="mes">
+              <div class="bubble_1" style="border-radius: 10px 10px 10px 0;background-color: #c0bfbf;">
+                {{info}}
+              </div>
+            </div>
+          </div>
+          <div class="p-talking_2" v-show="show">
+            <div class="mes" style="justify-content: flex-end">
+              <div class="bubble_1" style="border-radius: 10px 10px 0 10px;margin-right: 20px;background-color: #22af22;">
+                {{user_info}}
+              </div>
+            </div>
+            <div class="ai-dot">
+              <img src="../../assets/user.png" alt="" loading="lazy">
+            </div>
+          </div>
+          <div class="input_2">
+            <div class="input_4">
+              <el-button round class="el-btn" style="width: 40px;height: 40px;background-color: #4361FCE5">
+                <el-icon style="color: white"><PictureRounded /></el-icon>
+              </el-button>
+            </div>
+            <div class="input_3">
+              <el-input clearable placeholder="请输入您的问题" v-model="message"/>
+            </div>
+            <div class="input_4">
+              <el-button round class="el-btn" style="width: 40px;height: 40px;background-color: #4361FCE5" @click="commit">
+                <el-icon style="color: white"><Position /></el-icon>
+              </el-button>
+            </div>
+          </div>
+        </div>
+        <div class="digital-human"></div>
+      </div>
+    </div>
   </div>
 </template>
 
